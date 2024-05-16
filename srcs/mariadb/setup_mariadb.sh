d9d96da7acfcfeb4bd1adba502d754a00ada9b52
Pac4games <paulo.nicolau7@gmail.com>
2024-05-16 15:15:59 +0100

    YEAAAAAAAH BABY, THAT'S WHAT I'VE BEEN WAITING FOR

--- srcs/mariadb/setup_mariadb.sh
+++ srcs/mariadb/setup_mariadb.sh
@@ -6,7 +6,7 @@ if [ ! -d /var/lib/mysql/$WP_DATABASE ]; then
 	mysql -u root -e "CREATE DATABASE $WP_DATABASE;" \
 	&& '/usr/bin/mysqladmin' -u root password $MARIADB_ROOT_PWD
 	mysql -u root -e "CREATE USER '"$DB_USER"'@'"$WP_CONT_NAME.$NETWORK_NAME"' IDENTIFIED BY '"$DB_PWD"'; GRANT ALL PRIVILEGES ON *.* TO '"$DB_USER"'@'"$WP_CONT_NAME.$NETWORK_NAME"'; FLUSH PRIVILEGES;"
-#	mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '"$MARIADB_ROOT_PWD"';"
+	mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '"$MARIADB_ROOT_PWD"';"
 	mysqladmin -u root -p $MARIADB_ROOT_PWD shutdown
 fi
 
